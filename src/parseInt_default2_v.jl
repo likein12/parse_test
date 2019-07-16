f9ec72576bac7449c6f7e075257b44d27545ece7
likein12 <44144095+likein12@users.noreply.github.com>
2019-07-16 18:15:11 +0900

    Update parseInt_default2_v.jl

--- src/parseInt_default2_v.jl
+++ src/parseInt_default2_v.jl
@@ -19,3 +19,7 @@ a = parseInt_default2(readline_org())
 @time for i = 1:a
 	b,c = map(parseInt_default2,split(readline_org()))
 end
+
+
+#parseInt_default2の部分についてはfrom:(@ sgryjp)
+#この関数の場合、改行コードを取り除く必要があるので、readline_org()関数を使用
